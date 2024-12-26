a4c5b66272b87f9fadd66cbd810ac471284d5f83
Антон Викторович <antontonik86@gmail.com>
2024-12-26 14:15:02 +0300

    Update module_9_7.py

--- module_9_7.py
+++ module_9_7.py
@@ -1,24 +1,27 @@
+
+
+def sum_three(a, b, c):
+    return a + b + c
+
+
 def is_prime(func):
     def wrapper(*args, **kwargs):
-        res = func(*args, **kwargs)
-        if res > 1:
-            for i in range(2, res + 1):
-                if res % i == 0:
-                    print("Составное")
-                    break
+        result = func(*args, **kwargs)
+        if result > 1:
+            if all((result % i != 0) for i in range(2, int(result ** 0.5) + 1)):
+                return "Простое"
             else:
-                print("Простое")
-        else:
-            print("Составное")
-
-        return res
+                return "Составное"
 
     return wrapper
 
 
 @is_prime
 def sum_three(a, b, c):
-    return a + b + c
+    result = a + b + c
+    print(result)
+    return result
 
-result = sum_three(2, 3, 6)
-print(result)
+
+res = sum_three(2, 3, 6)
+print(res)
