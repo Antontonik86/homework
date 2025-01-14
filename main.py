94960811332a8cf0989aa6e838f8673364ebd048
Антон Викторович <antontonik86@gmail.com>
2025-01-14 17:47:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from routers import task, user
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True}, debug=True)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get('/')
+async def welcome():
+    return {"message": "Welcome to Taskmanager"}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+app.include_router(user.router)
+app.include_router(task.router)
