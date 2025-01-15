3a1be74b6860fe6e59794002458251115b26d1d6
Антон Викторович <antontonik86@gmail.com>
2025-01-15 14:57:14 +0300

    Update module_13_5.py
    подправил код

--- module_13_5.py
+++ module_13_5.py
@@ -4,10 +4,9 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-
 import asyncio
 
-api = ""
+api = " "  
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
@@ -20,70 +19,67 @@ kb = ReplyKeyboardMarkup(
     ], resize_keyboard=True
 )
 
-
-
-
-
 @dp.message_handler(text='Информация')
-async def inform(message):
-    await message.answer('Привет этот бот создан по формуле Миффлина-Сан Жеора – это одна из самых последних формул расчета калорий для '
+async def inform(message: types.Message):
+    await message.answer('Привет! Этот бот создан по формуле Миффлина-Сан Жеора – это одна из самых последних формул расчета калорий для '
                          'оптимального похудения или сохранения нормального веса. Она была выведена в 2005 году и '
-                         'все чаще стала заменять классическую формулу Харриса-Бенедикта.')
+                         'все чаще заменяет классическую формулу Харриса-Бенедикта.')
 
 class UserState(StatesGroup):
     age = State()
     growth = State()
     weight = State()
 
-
-
-@dp.message_handler(text = 'Расчитать')
-async def set_age(message):
+@dp.message_handler(text='Рассчитать')
+async def set_age(message: types.Message):
     await message.answer('Введите свой возраст: ')
     await UserState.age.set()
 
-@dp.message_handler(state= UserState.age)
-async def set_growth(message, state):
-    await state.update_data(age = message.text)
-    await message.answer('Введите свой рост: ')
-    await UserState.growth.set()
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    if message.text.isdigit():  
+        await state.update_data(age=message.text)
+        await message.answer('Введите свой рост в см: ')
+        await UserState.growth.set()
+    else:
+        await message.answer('Пожалуйста, введите корректный возраст в числах.')
 
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    if message.text.isdigit():  
+        await state.update_data(growth=message.text)
+        await message.answer('Введите свой вес в кг:')
+        await UserState.weight.set()
+    else:
+        await message.answer('Пожалуйста, введите корректный рост в числах.')
 
-@dp.message_handler(state= UserState.growth)
-async def sey_weight(message, state):
-    await state.update_data(growth = message.text)
-    await message.answer('Введите свой вес:')
-    await UserState.weight.set()
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    if message.text.replace('.', '', 1).isdigit():  
+        await state.update_data(weight=message.text)
+        data = await state.get_data()
 
+        age = float(data['age'])
+        growth = float(data['growth'])
+        weight = float(data['weight'])
 
-@dp.message_handler(state= UserState.weight)
-async def send_calories(message, state):
-    await state.update_data(weight = message.text)
-    data = await state.get_data()
+        man = 10 * weight + 6.25 * growth - 5 * age + 5
+        woman = 10 * weight + 6.25 * growth - 5 * age - 161
 
-    age = float(data['age'])
-    growth = float(data['growth'])
-    weight = float(data['weight'])
+        await message.answer(f"Норма калорий для мужчин: {man:.2f} ккал")
+        await message.answer(f"Норма калорий для женщин: {woman:.2f} ккал")
 
-    man = 10 * weight + 6.25 * growth - 5 * age + 5
-    woman = 10 * weight + 6.25 * growth - 5 * age - 161
-
-    await message.answer(f"Норма калорий для мужчин: {man}")
-    await message.answer(f"Норма калорий для женщин: {woman}")
-
-    await state.finish()
-
+        await state.finish()
+    else:
+        await message.answer('Пожалуйста, введите корректный вес в числах.')
 
 @dp.message_handler(commands=["start"])
-async def start(message):
-    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
-
+async def start(message: types.Message):
+    await message.answer('Привет! Я бот, помогающий твоему здоровью.', reply_markup=kb)
 
 @dp.message_handler()
-async def all_massages(message):
+async def all_messages(message: types.Message):
     await message.answer('Введите команду /start, чтобы начать общение.')
-
-
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
